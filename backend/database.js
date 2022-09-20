1c6d77b96470ae4bfa51fd15b89a21fa3ac11537
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2022-09-20 14:03:59 +0530

    database function changes

--- backend/database.js
+++ backend/database.js
@@ -121,10 +121,10 @@ const readStudentInfo = async (id) => {
 }
 
 const addStudent = async (id, name, age, religion) => {
-    const sql = `INSERT INTO student(id,name,age) values(?,?,?)`
+    const sql = `INSERT INTO student(id,name,age,religion) values(?,?,?,?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [id,name,age])
+            .raw(sql, [id,name,age,religion])
             .then((student) => {
                 resolve(student);
             })
@@ -135,10 +135,10 @@ const addStudent = async (id, name, age, religion) => {
 }
 
 const updateStudent = async (name, age, religion, id) => {
-    const sql = `UPDATE student SET name=?, age=?, WHERE id=?`
+    const sql = `UPDATE student SET name=?, age=?, religion=?, WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name,age,id])
+            .raw(sql, [name,age,religion,id])
             .then(() => {
                 resolve({status:"Successfully updated Teacher"});
             })
